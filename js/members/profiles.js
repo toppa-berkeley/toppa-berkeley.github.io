70fc10fefd0330c684fdd4e4344cedbd0b97d9a9
Elvis Wei <ewei068@berkeley.edu>
2020-10-24 13:56:43 -0500

    Added profiles

--- js/members/profiles.js
+++ js/members/profiles.js
@@ -1,3 +1,9 @@
+function createSectionHeader(header) {
+  document.write('<div class="grid_12">' +
+        '<h2>' + header + '</h2>' +
+      '</div>');
+}
+
 function createProfile(img_src, name, description) {
   document.write('<div class="grid_3">' +
       '<div class="box", style="border-radius: 20px">' +
@@ -9,9 +15,51 @@ function createProfile(img_src, name, description) {
         '</div>' +
       '</div>');
 }
-for (var i = 0; i < 12; i++) {
-  var img_src = "images/profile.png";
-  var name = "Name " + i;
-  var description = "Description " + i;
-  createProfile(img_src, name, description);
+
+function createSection(header) {
+  createSectionHeader(header);
+
+  for (const person of response) {
+    var img_src = "images/profile.png";
+    var name = person["First Name"] + " " + person["Last Name"];
+    // var description = "Description ";
+    var description = person["A brief introduction about yourself"];
+    var team = person["Project Team"];
+    // console.log(description);
+
+    if (team == header) {
+      createProfile(img_src, name, description);
+    }
+  }
+}
+
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'js/members/toppa-test.json', false);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(JSON.parse(xobj.responseText));
+    }
+  };
+  xobj.send(null);
+}
+
+var response;
+loadJSON(function(json) {
+  console.log(json);
+  response = json;
+});
+
+var teams = ['Member & Resource Directory',
+'Expanding Your Horizons',
+'NGO Consulting for Pandemic Professors',
+'Me @ ...',
+'Project Content Kickstart',
+'Ecology Center',
+'Newsletter & Toppa E-book',
+'AI + Stock Trading'];
+
+for (const team of teams) {
+  createSection(team);
 }
